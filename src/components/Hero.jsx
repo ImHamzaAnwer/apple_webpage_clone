bb5e146f500a01330ab4b14a0a3aeb12fdda6546
Hamza Anwer <hamza.anwer8@gmail.com>
2024-04-24 03:43:32 +0500

    cta button position fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
   useGSAP(() => {
     gsap.to("#hero", { opacity: 1, delay: 2 });
-    gsap.to("#cta", { opacity: 1, delay: 2, y: -10 });
+    gsap.to("#cta", { opacity: 1, delay: 2, y: -50 });
   }, []);
 
   return (
